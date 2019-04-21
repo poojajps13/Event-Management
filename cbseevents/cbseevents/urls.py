d8208b220d9875890e7ff377c0d3ed9050f7acff
Pranjal Kushwaha <pranjal0819@gmail.com>
2019-04-21 14:31:51 +0530

    Home Page

--- cbseevents/cbseevents/urls.py
+++ cbseevents/cbseevents/urls.py
@@ -15,16 +15,6 @@ urlpatterns = [
     re_path('froala_editor/', include('froala_editor.urls')),
 
     # path('set', set_user),
-    path('VLSI_Design/', vlsi_design, name='vlsi_design'),
-    path('Big_Data_Analytics/', big_data, name='big_data'),
-    path('SMC_India_PvtLtd./', smc_india, name='smc_india'),
-    path('Texas_Instruments_Embedded_System_Lab/', texas, name='texas'),
-    path('ABES_NI_Innovation_Centre/', innovation_centre, name='innovation_centre'),
-    path('Software_Development/', software_development, name='software_development'),
-    path('Structural_Design_and_Analysis/', structural_design, name='structural_design'),
-    path('Mobile_Application_Development/', mobile_application, name='mobile_application'),
-    path('Cisco_Networking_Academy/', cisco_networking_academy, name='cisco_networking_academy'),
-    path('Industrial_Automation_Research_&_Training_Centre_(IARTC)/', automation_research, name='automation_research'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
