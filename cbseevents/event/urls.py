88affaf67069e3b4e7d5c2651b06ea8031bcd961
Pranjal Kushwaha <pranjal0819@gmail.com>
2019-11-05 18:15:32 +0530

    Update

--- cbseevents/event/urls.py
+++ cbseevents/event/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import AddEvent, EventDetail, UpdateEvent, DeleteEvent, EventListView
 
 urlpatterns = [
-    path('event-list', EventListView.as_view(), name='event_list'),
+    path('', EventListView.as_view(), name='event_list'),
     path('add-event', login_required(AddEvent.as_view()), name='add_event'),
     path('<slug>/event-detail', EventDetail.as_view(), name='event_detail'),
     path('<slug>/update-event', login_required(UpdateEvent.as_view()), name='update_event'),
