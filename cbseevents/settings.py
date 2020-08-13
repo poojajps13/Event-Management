25ec4fc2429efca3e5476dcceb70558329fdc947
pranjal0819 <pranjal0819@gmail.com>
2020-08-13 17:15:21 +0530

    Deploy on heroku

--- cbseevents/settings.py
+++ cbseevents/settings.py
@@ -129,8 +129,9 @@ STATICFILES_DIRS = [
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR,"static/")
 
+# Media File (Image, Video)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 # Login Path
 
